111965bb3f7458fafe6408ca85902b2339f12460
bm13563 <bm13563@my.bristol.ac.uk>
2020-07-01 14:00:16 +0100

    trying iife format

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ export default [{
   input: 'src/randomPolys.js',
   output: {
     file: 'build/bundle.js',
-    format: 'es'
+    format: 'iife',
+    name: "RandomPolys"
   },
   plugins: [
     babel({ babelHelpers: 'bundled' })
@@ -14,7 +15,8 @@ export default [{
     input: 'src/demo/demo.js',
     output: {
       file: 'src/demo/bundle.js',
-      format: 'es'
+      format: 'iife',
+      name: "RandomPolys"
     },
     plugins: [
       babel({ babelHelpers: 'bundled' })
